40902e5d995a6cadf060bb16a7ae891ec574643b
Elodie Manai <elodie@linkio.net>
2022-02-19 19:55:06 +0100

    pull

--- ScoreGrid.h
+++ ScoreGrid.h
@@ -52,158 +52,6 @@ class ScoreGrid
 
             switch (indexGrid)
             {
-<<<<<<< HEAD
-            // Top grid :
-            case 1:
-            {
-                for (unsigned i = 0; i < sizeOfDices; i++)
-                    if (dices[i] == 1)
-                        pts += 1;
-            }
-            break;
-            case 2:
-            {
-                for (unsigned i = 0; i < sizeOfDices; i++)
-                    if (dices[i] == 2)
-                        pts += 2;
-            }
-            break;
-            case 3:
-            {
-                for (unsigned i = 0; i < sizeOfDices; i++)
-                    if (dices[i] == 3)
-                        pts += 3;
-            }
-            break;
-            case 4:
-                for (unsigned i = 0; i < sizeOfDices; i++)
-                    if (dices[i] == 4)
-                        pts += 4;
-                break;
-            case 5:
-            {
-                for (unsigned i = 0; i < sizeOfDices; i++)
-                    if (dices[i] == 5)
-                        pts += 5;
-            }
-            break;
-            case 6:
-            {
-                for (unsigned i = 0; i < sizeOfDices; i++)
-                    if (dices[i] == 6)
-                        pts += 6;
-            }
-            break;
-
-            // Bottom grid :
-            case 7: // Brelan
-            {
-                for (unsigned i = 1; i < 7; i++)
-                {
-                    unsigned compt = 0;
-                    for (unsigned j = 0; j < sizeOfDices - 3; j++)
-                    {
-                        if (dices[j] == i)
-                            compt++;
-                    }
-                    if (compt >= 3)
-                    {
-                        isValid = true;
-                        break;
-                    }
-                }
-                if (isValid)
-                    for (unsigned i = 0; i < sizeOfDices; i++)
-                        pts += dices[i];
-            }
-
-            break;
-            case 8: // Carre
-            {
-                for (unsigned i = 1; i < 7; i++)
-                {
-                    unsigned compt = 0;
-                    for (unsigned j = 0; j < sizeOfDices - 3; j++)
-                    {
-                        if (dices[j] == i)
-                            compt++;
-                    }
-                    if (compt >= 4)
-                    {
-                        isValid = true;
-                        break;
-                    }
-                }
-                if (isValid)
-                    for (unsigned i = 0; i < sizeOfDices; i++)
-                        pts += dices[i];
-            }
-            break;
-            case 9: // Petite suite
-            {
-                unsigned cmpt = 0;
-                for (unsigned i = 1; i < 7 && cmpt < 3; i++) // Valeur du dés
-                {
-                    isValid = false;
-                    for(unsigned j = 0; j < sizeOfDices; j++) {
-                        if(dices[j] == i) {
-                            isValid = true;
-                            cmpt++;
-                        }
-                    }
-                    if(!isValid && i > 1) break;
-                    else if (isValid && cmpt > 3) {
-                        pts = 30;
-                        break;
-                    }
-                }
-
-            }
-            break;
-            case 10: // Grande Suite
-            {
-                this->sortArray(dices, sizeOfDices);
-                for (unsigned i = 0; i < sizeOfDices; i++)
-                {
-                    if (dices[i + 1] && dices[i] == dices[i + 1] || dices[i] + 1 != dices[i+1])
-                        break;
-                }
-                pts += 40;
-            }
-            break;
-            case 11: // Full
-            {
-                this->sortArray(dices, sizeOfDices);
-                if((dices[0] == dices[1] && dices[1] == dices[2]) && (dices[3] == dices[4]) || 
-                    (dices[2] == dices[3] && dices[3] == dices[4]) && (dices[0] == dices[1])) pts = 25;
-            }
-            break;
-            case 12: // Yahtzee
-            {
-                unsigned lastDice = dices[0];
-                for (unsigned i = 1; i < sizeOfDices; i++)
-                {
-                    if (dices[i] != lastDice){
-                        break;
-                        isValid = false;
-                    }
-                }
-                if (isValid)
-                    pts = 50;
-            }
-            break;
-            case 13: // Chance
-            {
-                for (unsigned i = 0; i < sizeOfDices; i++) pts += dices[i];
-            }
-            break;
-
-            default:
-                pts = 0;
-                break;
-            }
-            printf("points : %d\n", pts);
-=======
                 int valueOfDice = 0;
                 // Top grid :
                 case 1:
@@ -282,7 +130,6 @@ class ScoreGrid
                     pts = -1;
                     break;
             }
->>>>>>> c0b8d87392eae6de9a9ee281b4a19630bc0e0e2a
             return pts;
         }
 
@@ -302,9 +149,6 @@ class ScoreGrid
                     }
                 }
             }
-<<<<<<< HEAD
-
-=======
             return 0;
         }
 
@@ -319,7 +163,6 @@ class ScoreGrid
                 
                 if (countOfIdentical >= numberOfIdenticalRequired) return 1;
             }
->>>>>>> c0b8d87392eae6de9a9ee281b4a19630bc0e0e2a
             return 0;
         }
 };
